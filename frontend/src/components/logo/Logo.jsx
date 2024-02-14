a2542fd01bddacfb03c580fe3c4e4f717869a274
cristianvasquezc <cristianvasquez1312@gmail.com>
2024-02-14 18:38:56 -0500

    Agregando componentes buttons, hero, logo, pet, se comento el codigo por defecto de page para mostrar hero

---
+++ frontend/src/components/logo/Logo.jsx
@@ -0,0 +1,74 @@
+"use client";
+import React from "react";
+import { useState, useEffect, useRef } from "react";
+
+const Logo = ({ size = "text-4xl  md:text-8xl" }) => {
+  const [displayedText, setDisplayedText] = useState("NEKODE");
+  const [hovered, setHovered] = useState(false);
+
+  const handleMouseOver = () => {
+    setHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setHovered(false);
+    setDisplayedText("NEKODE");
+  };
+
+  useEffect(() => {
+    if (hovered) {
+      const letters = "1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      let iteration = 0;
+      const interval = setInterval(() => {
+        const newText = displayedText
+          .split("")
+          .map((letter, index) => {
+            if (letter === " ") {
+              return " ";
+            }
+            return letters[Math.floor(Math.random() * letters.length)];
+          })
+          .join("");
+
+        setDisplayedText(newText);
+
+        if (iteration >= displayedText.length) {
+          clearInterval(interval);
+        }
+
+        iteration += 1 / 3;
+      }, 30);
+
+      return () => clearInterval(interval);
+    }
+  }, [hovered, displayedText]);
+
+  return (
+    <div
+      onMouseOver={handleMouseOver}
+      onMouseLeave={handleMouseLeave}
+      className="flex items-center justify-center gap-x-2.5 lg:gap-x-5 text-white"
+    >
+      {displayedText.split("").map((letter, index) => (
+        <div
+          key={index}
+          className={`${size} p-2 pt-4 flex justify-center items-center border-gray-500 ${
+            letter === " " ? "none" : "border"
+          }`}
+        >
+          {letter === " " ? (
+            <span>&nbsp;</span>
+          ) : (
+            <span
+              className={`${hovered === true ? "text-gray-500" : "text-white"}`}
+            >
+              {letter}
+            </span>
+          )}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Logo;
