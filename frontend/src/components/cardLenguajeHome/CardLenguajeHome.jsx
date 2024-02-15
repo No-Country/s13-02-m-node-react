78fdf0ec86defee5a5310e443a4739b63f5a8a1c
martinasorialanzi <martinasorialanzi@gmail.com>
2024-02-15 18:43:12 -0300

    cambios cards etapas home

---
+++ frontend/src/components/cardLenguajeHome/CardLenguajeHome.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Card, CardContent, Typography } from '@mui/material';
+
+const CardLenguajeHome = () => {
+  return (
+    <Card sx={{ maxWidth: 631, height: 146, backgroundColor: '#333333', color: 'white',borderRadius: ["19px"],
+    boxShadow: "0px 0px 8px 0px rgba( 31, 38, 135, 1 )", }}>
+    <CardContent>
+      <Typography variant="h5" component="h2" className='mb-1' >
+        JavaScript
+      </Typography>
+      <Typography variant="body2" color="#FFFFFF">
+        JavaScript is a programming language used in web development to add interactivity to web pages. Its simple syntax and extensive ecosystem of tools make it essential in creating dynamic user experiences.
+      </Typography>
+    </CardContent>
+  </Card>
+  );
+};
+
+export default CardLenguajeHome;
