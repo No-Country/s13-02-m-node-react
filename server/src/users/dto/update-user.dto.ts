5a839a42d3db5333a58884cf977d6b2803038526
Maria Villen <9887543+MariaVillen@users.noreply.github.com>
2024-02-25 21:01:05 +0100

    user update

--- server/src/users/dto/update-user.dto.ts
+++ server/src/users/dto/update-user.dto.ts
@@ -3,39 +3,40 @@ import {
   IsEnum,
   IsInt,
   IsNotEmpty,
+  IsOptional,
   IsString,
 } from 'class-validator';
 import { NOTIFICATIONFREQUENCY } from '../../config/constants/notification_frequency';
 import { ROLES } from '../../config/constants/roles';
 
 export class UpdateUserDto {
+  @IsOptional()
   @IsNotEmpty()
   @IsString()
   username: string;
 
-  // @IsNotEmpty()
-  // @IsEmail()
-  // email: string;
-
-  // @IsNotEmpty()
-  // @IsStrongPassword()
-  // password: string;
-
-  // @IsString()
-  // tokenPass: string;
-
+  @IsOptional()
+  @IsNotEmpty()
   @IsEnum(ROLES)
   role: ROLES;
 
+  @IsOptional()
+  @IsNotEmpty()
   @IsInt()
   life: number;
 
+  @IsOptional()
+  @IsNotEmpty()
   @IsInt()
   totalPoints: number;
 
+  @IsOptional()
+  @IsNotEmpty()
   @IsEnum(NOTIFICATIONFREQUENCY)
   challengeNotification: NOTIFICATIONFREQUENCY;
 
+  @IsOptional()
+  @IsNotEmpty()
   @IsBoolean()
   notification: boolean;
 }
