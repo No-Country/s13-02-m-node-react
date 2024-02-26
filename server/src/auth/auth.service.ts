d087d2210bc558411257792fbb4ce7ae44df5892
TorresRichardtorrot <torresdrichar@gmail.com>
2024-02-26 17:04:30 -0400

    endPoint questions

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
 import { LoginAuthDto } from './dto/login-auth.dto';
 import { RegisterAuthDto } from './dto/register-auth.dto';
