1153e58bf10853ecc07b00a815681cd6a52ca76c
Marvin Ramos <mr487223@gmail.com>
2024-02-27 11:44:07 -0600

    add notidications folder

---
+++ server/src/notifications/notification.module.ts
@@ -0,0 +1,23 @@
+/* eslint-disable prettier/prettier */
+import { Module } from '@nestjs/common';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { EjsAdapter } from '@nestjs-modules/mailer/dist/adapters/ejs.adapter';
+
+@Module({
+  imports: [
+    MailerModule.forRoot({
+      transport: 'smtps://user@domain.com:pass@smtp.domain.com',
+      defaults: {
+        from: '"nest-modules" <modules@nestjs.com>',
+      },
+      template: {
+        dir: __dirname + '/templates',
+        adapter: new EjsAdapter(),
+        options: {
+          strict: true,
+        },
+      },
+    }),
+  ],
+})
+export class AppModule {}
---
+++ server/src/notifications/notification.service.ts
@@ -0,0 +1,24 @@
+/* eslint-disable prettier/prettier */
+import { Injectable } from '@nestjs/common';
+import { MailerService } from '@nestjs-modules/mailer';
+
+@Injectable()
+export class ExampleService {
+  constructor(private readonly mailerService: MailerService) {}
+  public example(): void {
+    this.mailerService
+      .sendMail({
+        to: 'test@nestjs.com',
+        from: 'noreply@nestjs.com',
+        subject: 'Testing Nest Mailermodule with template ✔',
+        template: __dirname + '/welcome', // The `.pug`, `.ejs` or `.hbs` extension is appended automatically.
+        context: {
+          // Data to be sent to template engine.
+          code: 'cf1a3f828287',
+          username: 'john doe',
+        },
+      })
+      .then(() => {})
+      .catch(() => {});
+  }
+}
