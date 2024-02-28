cb5d2dc8a23179d12bb9b8baf51a929cae7d8f16
facundo000 <facundo.guzman@mi.unc.edu.ar>
2024-02-28 11:13:01 -0300

    upload image - ADMIN

--- server/src/files/files.controller.ts
+++ server/src/files/files.controller.ts
@@ -1,17 +1,15 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UploadedFile, UseInterceptors, BadRequestException, Res, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UploadedFile, UseInterceptors, BadRequestException, Res, Req, UseGuards, UnauthorizedException, InternalServerErrorException } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { ConfigService } from '@nestjs/config';
 import { Response } from 'express';
 import { Multer, diskStorage } from 'multer';
 import { FilesService } from './files.service';
-
 import { fileFilter, fileNamer } from './helpers';
 import { UsersService } from '../users/users.service';
-import { UpdateUserDto } from '../users/dto/update-user.dto';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
-import { AuthGuard, RolesGuard } from 'src/auth';
+import { ApiBearerAuth } from '@nestjs/swagger';
+import { AuthGuard } from 'src/auth';
+import { UpdateUserDto } from 'src/users/dto/update-user.dto';
 
-@ApiTags('users')
 @ApiBearerAuth()
 @UseGuards(AuthGuard)
 @Controller('files')
@@ -35,48 +33,50 @@ export class FilesController {
     return path;
   }
 
-  @Post(':userId/avatar')
-  @UseInterceptors( FileInterceptor('file', {
+  @Patch(':userId/avatar')
+  @UseInterceptors(FileInterceptor('file', {
     fileFilter: fileFilter,
-    // limits: { fileSize: 4000 },
     storage: diskStorage({
       destination: './static/avatars',
       filename: fileNamer
     })
-  }))
-  async uploadAvatarImage(
-    @UploadedFile() file: Express.Multer.File,
-    @Param('userId') userId: string,
-    @Req() req
-    ){
-      if( !file ) {
-        throw new BadRequestException('Make sure that the file  is an image');
-      }
-      
-      const secureUrl = `${ this.configService.get('HOST_API') }/files/${userId}/avatar/${ file.filename } `;
-
-      const user = req.user;
-      const userAuth = { role: user.role, id: user.id };
-      
-      if(userId !== user.id) {
-        throw new BadRequestException('User ID in the URL does not match the authenticated user ID');
-      }
-
-      const userEntity = await this.usersService.findUserById(user.id);
-
-      const updateUserDto: UpdateUserDto = {
-        username: userEntity.username,
-        role: userEntity.role,
-        life: userEntity.life,
-        totalPoints: userEntity.totalPoints,
-        challengeNotification: userEntity.challengeNotification,
-        notification: userEntity.notification,
-        avatarUrl: secureUrl,
-      };
-
-      await this.usersService.update(user.id, updateUserDto, userAuth);
-      
-      return { secureUrl };
+}))
+async uploadAvatarImage(
+  @UploadedFile() file: Express.Multer.File,
+  @Param('userId') userId: string,
+  @Req() req
+) {
+  if (!file) {
+    throw new BadRequestException('Make sure that the file is an image');
   }
+
+  const secureUrl = `${this.configService.get('HOST_API')}/files/avatar/${file.filename}`;
+
+  const user = req.userAuth;
+  // console.log(user)
+  if (!user) {
+    console.log('User is not authenticated');
+    throw new UnauthorizedException('User is not authenticated');
+  }
+  if (userId !== user.id) {
+    throw new BadRequestException('User ID in the URL does not match the authenticated user ID');
+  }
+
+    const userEntity = await this.usersService.findUserById(user.id);
+
+    const updateUserDto: UpdateUserDto = {
+      username: userEntity.username,
+      role: userEntity.role,
+      life: userEntity.life,
+      totalPoints: userEntity.totalPoints,
+      challengeNotification: userEntity.challengeNotification,
+      notification: userEntity.notification,
+      avatarUrl: secureUrl,
+    };
+
+    await this.usersService.update(user.id, updateUserDto, user);
+
+    return { avatarUrl: secureUrl };
+}
 
 }
