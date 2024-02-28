3b0f8d52987459c21cd926f8ec22732ff98671a0
martinasorialanzi <martinasorialanzi@gmail.com>
2024-02-28 12:12:35 -0300

    Seleccion de lenguaje

--- frontend/src/components/buttons/Languages.jsx
+++ frontend/src/components/buttons/Languages.jsx
@@ -14,22 +14,34 @@ const LanguageButton = styled(Button)({
 })
 
 const Languages = ({ data, onLanguageSelect}) => {
+  const [selectedLanguage, setSelectedLanguage] = useState();
 
   const handleButtonClick = (languageId) => {
     onLanguageSelect(languageId);
+    setSelectedLanguage(languageId);
   };
+  useEffect(() => {
+    if (!data || data.length === 0 || selectedLanguage) {
+      return;
+    }
 
+    setSelectedLanguage(data[0].id);
+  }, [data, selectedLanguage]);
+
+  if (!data) {
+    return <div>Loading...</div>;
+  }
   return (
     <div className='mb-3'>
       <Stack spacing={2} direction='row'>
         {data.map((res) => (
           <LanguageButton
-            key={res.name}
+            key={res.id}
             variant='outlined'
              onClick={() => handleButtonClick(res.id)}
-            //  style={{
-            //    backgroundColor: selectedLanguage === res.name ? '#d946ef' : ''
-            //  }}
+             style={{
+               backgroundColor: selectedLanguage === res.id ? '#d946ef' : ''
+             }}
           >
             {res.name}
           </LanguageButton>
