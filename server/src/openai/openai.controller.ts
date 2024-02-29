b87c040de5550e6b28c9081a21e10c77c0741efc
Maria Villen <9887543+MariaVillen@users.noreply.github.com>
2024-02-29 11:36:15 +0100

    format prettier

--- server/src/openai/openai.controller.ts
+++ server/src/openai/openai.controller.ts
@@ -3,7 +3,7 @@ import { Body, Controller, HttpStatus, Post } from '@nestjs/common';
 import { OpenaiService } from './openai.service';
 import { GetQuestionDto } from './dto/getQuestion.dto';
 import { CorrectQuestionDto } from './dto/correctQuestion.dto';
-import { ApiBody, ApiResponse,ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('OpenAi')
 @Controller('openai')
@@ -11,15 +11,21 @@ export class OpenaiController {
   constructor(private readonly openaiService: OpenaiService) {}
 
   @Post('/question')
-  @ApiResponse({status :HttpStatus.CREATED, description:`[{"title": "string"}]`})
-  @ApiBody({type: GetQuestionDto})
+  @ApiResponse({
+    status: HttpStatus.CREATED,
+    description: `[{"title": "string"}]`,
+  })
+  @ApiBody({ type: GetQuestionDto })
   async getQuestion(@Body() petition: GetQuestionDto): Promise<string[]> {
     return await this.openaiService.getQuestion(petition);
   }
 
   @Post('/correct')
-  @ApiResponse({status :HttpStatus.CREATED, description:`{"isCorrect": Boolean,"feedback": "string"}`})
-  @ApiBody({type: CorrectQuestionDto})
+  @ApiResponse({
+    status: HttpStatus.CREATED,
+    description: `{"isCorrect": Boolean,"feedback": "string"}`,
+  })
+  @ApiBody({ type: CorrectQuestionDto })
   async correctQuestion(@Body() petition: CorrectQuestionDto): Promise<object> {
     return await this.openaiService.correctQuestion(petition);
   }
