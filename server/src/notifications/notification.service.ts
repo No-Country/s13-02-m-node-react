947d4165b144f11e6ea35c182a0d68e5783f9bad
Marvin Ramos <mr487223@gmail.com>
2024-02-29 10:00:33 -0600

    set up notification schedule feature

--- server/src/notifications/notification.service.ts
+++ server/src/notifications/notification.service.ts
@@ -3,33 +3,96 @@ import { Injectable } from '@nestjs/common';
 import { MailerService } from '@nestjs-modules/mailer';
 import { MailDto } from './dto/mail.dto';
 import { ConfigService } from '@nestjs/config';
+import * as cron from 'cron';
+
+import { UsersService } from '../users/users.service';
+import { ErrorManager } from '../utils/error.manager';
+import Handlebars from 'handlebars';
+import * as fs from 'fs';
 
 @Injectable()
 export class EmailService {
+
   constructor(
     private readonly mailerService: MailerService,
-    private readonly configService: ConfigService
-    ) {}
-  public async sendEmail(mailDto: MailDto){
-    try{
-      const { email, name, subject } = mailDto;
-   
+    private readonly configService: ConfigService,
+    private readonly usersService: UsersService
+  ) {
+    this.setUpNotificationSchedule();
+  }
+
+  public async sendEmail(mailDto: MailDto): Promise<string>{
+    try {
+      const { email, name, subject, message} = mailDto;
+      const templatePath = 'src/notifications/template/index.hbs';
+
+      if (!fs.existsSync(templatePath)) {
+        throw new Error('Email template file not found.');
+      }
+
+      const emailTemplateSource = fs.readFileSync(templatePath, 'utf8');
+      const template = Handlebars.compile(emailTemplateSource);
+      
       await this.mailerService.sendMail({
         from: `Nekode 📧🐈‍⬛ <${this.configService.get('MAIL_USERNAME')}>`,
         to: email,
         subject: subject,
-        template: 'mail',
-        // context: {
-        //   name,
-        //   message
-        // },
+        html: template({ name, message }),
       });
       
-      return name + email;
-    }catch(error){
+      return "Email sent successfully! 📧🐈‍⬛👍🏼";
+    } catch(error) {
       console.log(error);
-      return error.message;
+      throw ErrorManager.createSignatureError(error.message);
     }
   }
-  
+
+  private async setUpNotificationSchedule() {
+    // 1 ==> Daily
+    // 7 ==> Weekly
+    // 0 ==> None
+    const defaultNotification: MailDto = {
+      email: '',
+      name: '',
+      subject: 'Daily Challenge Reminder 📅',
+      message: 'This is a reminder to complete your daily challenge!'
+    };
+
+    const emailsToSend = {
+      daily: [],
+      weekly: []
+    };
+
+
+    await this.usersService.findAll().then((users) => {
+      users.forEach((user) => {
+        const { challengeNotification } = user;
+        
+        if (challengeNotification === 1) {
+          defaultNotification.email = user.email;
+          defaultNotification.name = user.username;
+          emailsToSend.daily.push(defaultNotification);
+        }
+        else if (challengeNotification === 7) {
+          defaultNotification.email = user.email;
+          defaultNotification.name = user.username;
+          defaultNotification.subject = 'Weekly Challenge Reminder 🐈‍⬛📅';
+          defaultNotification.message = 'This is a reminder to complete your weekly challenge!';
+          emailsToSend.weekly.push(defaultNotification);
+        }
+      });
+    });
+    // */5 * * * * * ==> every 5 seconds
+    const dailyReminder = new cron.CronJob('0 0 * * *', async () => {
+      emailsToSend.daily.forEach(async (email) => {
+        await this.sendEmail(email).then((res) => { console.log(res); });
+      });
+    }, null, true, 'UTC');
+
+    const weeklyReminder = new cron.CronJob('0 0 * * 1', async () => {
+      emailsToSend.weekly.forEach(async (email) => {
+        await this.sendEmail(email);
+      });
+    }, null, true, 'UTC');
+  }
 }
