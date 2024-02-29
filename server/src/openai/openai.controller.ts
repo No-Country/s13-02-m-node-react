c565a3252b9b10d1643830d497f7a9d1a0447540
Maria Villen <9887543+MariaVillen@users.noreply.github.com>
2024-02-29 11:40:43 +0100

    format changing

--- server/src/openai/openai.controller.ts
+++ server/src/openai/openai.controller.ts
@@ -1,8 +1,8 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, HttpStatus, Post, UseGuards } from '@nestjs/common';
 import { OpenaiService } from './openai.service';
 import { GetQuestionDto } from './dto/getQuestion.dto';
 import { CorrectQuestionDto } from './dto/correctQuestion.dto';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiBody, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { RolesGuard } from 'src/auth/guards/roles.guard';
 import { AuthGuard } from 'src/auth/guards/auth.guards';
 
